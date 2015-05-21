e9c90eddb76ec68812ab465077f6fc03c030cafa
Adam Gleichsner <amg188@case.edu>
2015-05-20 23:49:50 -0400

    Added Crashlytics

--- HackCWRU App/AppDelegate.swift
+++ HackCWRU App/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //Crashylytics hook, always keep at end
+        Crashlytics.startWithAPIKey("479c2e2984369c33402ef64007433deaa5acebe9")
         return true
     }
 
