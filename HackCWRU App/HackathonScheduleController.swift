184c3b8ad240a4affee76bbc6b90517530e51661
Adam Gleichsner <amg188@case.edu>
2015-05-23 03:54:18 -0400

    Added the tab bar controller that will hold all of the view controllers for the user options. I'll clean this shit up once the static pages page view controller works

---
+++ HackCWRU App/HackathonScheduleController.swift
@@ -0,0 +1,28 @@
+//
+//  HackathonScheduleController.swift
+//  HackCWRU App
+//
+//  Created by Adam Gleichsner on 5/23/15.
+//  Copyright (c) 2015 CWRU Hacker Society. All rights reserved.
+//
+
+import UIKit
+
+class HackathonScheduleController: UITableViewController {
+    
+    let pageIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        println("Hello, hackers!")
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
