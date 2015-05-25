b5b8e036d47b330e0c7161ca3158767bea7cf710
Andrew Mason <mason@case.edu>
2015-05-24 21:10:26 -0700

    adding announcements controller

---
+++ HackCWRU App/AnnouncementsController.swift
@@ -0,0 +1,14 @@
+//
+//  AnnouncementsController.swift
+//  HackCWRU
+//
+//  Created by Andrew Mason on 5/24/15.
+//  Copyright (c) 2015 CWRU Hacker Society. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class AnnouncmentsController: UIViewController {
+//    <#properties and methods#>
+}
