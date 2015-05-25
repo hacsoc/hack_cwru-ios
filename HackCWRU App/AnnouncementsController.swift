3b3603ece15c03e419e02840a463c5f30430539c
Andrew Mason <mason@case.edu>
2015-05-24 22:22:10 -0700

    oh look, JSON! and hooking up to a backend!

--- HackCWRU App/AnnouncementsController.swift
+++ HackCWRU App/AnnouncementsController.swift
@@ -10,5 +10,29 @@ import UIKit
 import Foundation
 
 class AnnouncmentsController: UIViewController {
-//    <#properties and methods#>
+    var frame:CGRect?
+    
+    init(options: [NSObject : AnyObject]?, frame:CGRect) {
+        super.init(nibName: nil, bundle: nil)
+        self.frame = frame
+        self.automaticallyAdjustsScrollViewInsets = false
+        self.view.autoresizingMask = UIViewAutoresizing.None
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        // TODO: use preprocessor flags to swap out a real URL for production.
+        // This requires a rails server to be hosting the backend on your dev machine.
+        let url = NSURL(string: "http://localhost:3000/announcements.json")
+        let request:NSURLRequest = NSURLRequest(URL: url!)
+        var response:AutoreleasingUnsafeMutablePointer<NSURLResponse?> = nil
+        var error:NSErrorPointer = nil
+        var data:NSData = NSURLConnection.sendSynchronousRequest(request, returningResponse: response, error: error)!
+        var json:NSArray =
+            NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: nil) as! NSArray
+    }
+    
 }
